dbc50e0d0614d4e7d4d7622525963b3cc56e3571
Daniel Djordjevic <ddjordjev@elior.net>
2024-02-25 21:19:55 +0100

    fix routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,7 @@ import { HostingsComponent } from './pages/hostings/hostings.component';
 import { WeddingDetailsComponent } from './pages/wedding-details.component';
 
 export const routes: Routes = [
+  { path: '', redirectTo: '/mariage', pathMatch: 'full' },
   { path: 'hebergements', component: HostingsComponent },
   { path: 'mariage', component: WeddingDetailsComponent },
 ];
